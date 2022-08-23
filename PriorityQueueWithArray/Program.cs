4262c3f3a1bce31ef14656c729f599c73dce1782
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-23 19:38:20 +0430

    Initial Repo

---
+++ PriorityQueueWithArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using PriorityQueueWithArray;
+
+var queue = new PriorityQueue(4);
+
+queue.Enqueue(10);
+queue.Enqueue(3);
+queue.Enqueue(8);
+queue.Enqueue(4);
+queue.Enqueue(4);
+
+
+//queue.Dequeue();
+//queue.Dequeue();
+//queue.Dequeue();
+//queue.Dequeue();
+//queue.Dequeue();
+
+Console.WriteLine("_________________________");
+
+queue.Print();
