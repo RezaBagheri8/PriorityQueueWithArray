e54436643aa0b37c183d36f465754493bf139fd6
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-24 08:51:27 +0430

    Add priority model to solution

---
+++ PriorityQueueWithArray/Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueueWithArray
+{
+    public class Model
+    {
+        public int value;
+        public int priority;
+
+        public Model(int theValue, int thePriority)
+        {
+            value = theValue;
+            priority = thePriority;   
+        }
+    }
+}
